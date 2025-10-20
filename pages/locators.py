9635c4004e114c1bccd4838e1e8cf7c3b96abff8
moiseev.vl <memmfizwork@gmail.com>
2025-10-20 19:44:26 +0300

    Added new locators to main page Added initial file to check login function

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.ID, 'login_link')
 
+class LoginPageLocators:
+    EMAIL_INPUT_LOGIN = (By.ID, 'id_login-username')
+    PASSWORD_INPUT_LOGIN = (By.ID, 'id_login-password')
+
+
+
---
+++ pages/login_page.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from .base_page import BasePage
+
+load_dotenv()
+LOGIN = os.environ["stepik_login"]
+PASSWORD = os.environ["stepik_password"]
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert True
+
+
+
+
