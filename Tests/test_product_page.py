396dac2a3f30be0aea710a70ebf2cc3f420619af
moiseev.vl <memmfizwork@gmail.com>
2025-10-27 17:21:53 +0300

    Improved promo_urls list. Transformed into generated script

--- Tests/test_product_page.py
+++ Tests/test_product_page.py
@@ -1,8 +1,13 @@
 import time
 from pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+promo_urls = [f'{product_base_link}?promo=offer{i}' for i in range(10)]
+
+@pytest.mark.parametrize('link', promo_urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_page()
